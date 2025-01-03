90599ad0fffa041821842afa954af129b30ae05a
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-03 12:41:34 +0530

    completed-Setting up a DB and connecting to Express server - Practice assignment #2

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express');
-const { resolve } = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const User = require('./schema.js');
 
+dotenv.config();
 const app = express();
-const port = 3010;
+app.use(express.json());
 
-app.use(express.static('static'));
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(()=>{
+  console.log(`Connected to database`);
+})
+.catch((error)=>{
+  console.error(`Error connecting to database, ${error}`)
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.post('/api/users', async(req,res)=>{
+  try{
+    const userData=req.body;
+    const newUser =new User(userData);
+    await newUser.save();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    res.status(201).json({message:"user created successfully"});
+  }
+  catch(error){
+    if(error.name==='ValidationError'){
+      res
+      .status(400)
+      .json({message:"validation error", details:error.message})
+    }else{
+      res.status(500).json({ message: 'Server error', details: error.message });
+    }
+  }
+})
+const PORT = 3010;
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
